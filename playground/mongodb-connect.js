740686e4f9c9694382fbd530d8034504d69aee52
Kapitan Rudobrody <kapitan.rudobrody@gmail.com>
2019-09-16 15:44:29 +0200

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connec to to mongodb server');
+  }
+  console.log('Connected to mongodb server');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'John',
+    age: 43,
+    location: 'Poland'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  db.close();
+});
